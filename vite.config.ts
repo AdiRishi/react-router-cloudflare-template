f53366fc24aec0c9ae013167a412aa0df4536eae
Adishwar Rishi <adiswa123@gmail.com>
2025-01-26 13:05:32 +1100

    Upgrade to tailwindcss v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { getLoadContext } from "./workers/load-context";
 
 export default defineConfig(() => ({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
   plugins: [
+    tailwindcss(),
     cloudflareDevProxy({
       getLoadContext,
     }),
